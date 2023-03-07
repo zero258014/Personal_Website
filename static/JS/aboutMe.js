a69229a0a8ce0cec81c2e79df203fd346672bacc
shen <zero258014@gmail.com>
2023-03-07 19:52:14 +0900

    about me page
    update

---
+++ static/JS/aboutMe.js
@@ -0,0 +1,15 @@
+// const p = document.querySelectorAll("p")
+
+// for (let i = 0; i < p.length; i++) {
+//     p[i].addEventListener("mouseover", () => {
+//         p[i].style.backgroundColor = "RGB(222,255,222)"
+//         p[i].style.fontWeight = "600"
+//     })
+// }
+
+// for (let i = 0; i < p.length; i++) {
+//     p[i].addEventListener("mouseleave", () => {
+//         p[i].style.backgroundColor = "azure"
+//         p[i].style.fontWeight = "400"
+//     })
+// }
